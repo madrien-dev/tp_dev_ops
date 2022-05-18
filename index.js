5d1275b68cc290f411de6a924b57de8e58290388
pierrechauvin <chauvinpierre79@gmail.com>
2022-05-18 10:34:19 +0200

    test unitaire

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => 
+    cdsc{}
     res.send(`Hello app, running on: ${process.env.NODE_ENV}`)
 })
 
